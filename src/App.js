8d736b8a975c1cd42045fed1085e8555b2e02732
saba-ab <sabaabjandadze@gmail.com>
2023-10-25 01:05:54 +0400

    updated build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useCallback, useState } from "react";
+import "./App.css";
+import PostLengthCalc from "./PostLengthCalc";
+import Post from "./Post";
 function App() {
+  const [input, setInput] = useState("");
+  const [posts, setPosts] = useState([]);
+  const [postLength, setPostLength] = useState(0);
+  const handleInput = (event) => {
+    setInput(event.target.value);
+  };
+  const callBackInput = useCallback(handleInput, []);
+  const handlePost = (e) => {
+    if (e.key === "Enter" && input.trim() !== "") {
+      setPosts([input, ...posts]);
+      setPostLength(postLength + input.length);
+      setInput("");
+    }
+  };
+  console.log("app shi shemovida");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onKeyDown={handlePost}
+        type="text"
+        value={input}
+        onChange={callBackInput}
+        style={{ backgroundColor: "yellow", height: "40px" }}
+        placeholder="enter post tittle"
+      />
+      <PostLengthCalc postLength={postLength} />
+      {posts.map((postText, i) => (
+        <Post key={i} text={postText} />
+      ))}
     </div>
   );
 }
